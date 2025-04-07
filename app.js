2cd143d733c2243d59ee8a15cf6b69eab3a17463
Rutger Speksnijder <rutgerspeksnijder@hotmail.com>
2025-04-07 22:40:47 +0200

    Remove jquery dependency and clean up

--- app.js
+++ app.js
@@ -6,59 +6,78 @@ const COPY_BUTTON_TEXT = 'Melding kopi&euml;ren';
 
 function initialize() {
     // Find all alert tables and add a copy button
-    $(document).find('table#alerts').each(function() {
-        $(this).find('tbody').append(`<tr class="custom-tr"><td colspan="3"></td><td><button class="copy-alert">${COPY_BUTTON_TEXT}</button></td></tr>`);
+    document.querySelectorAll('table#alerts').forEach(elem => {
+        const tableRow = document.createElement('tr');
+        tableRow.classList.add('custom-tr');
+        tableRow.innerHTML = `<td colspan="3"></td><td><button class="copy-alert">${COPY_BUTTON_TEXT}</button></td>`;
+        elem.querySelector('tbody').append(tableRow);
     });
 }
 
-$(document).ready(function() {
+// Initialize
+initialize();
+
+// Create an observer to check for reload of alerts
+const observer = new MutationObserver(function(mutations, observer) {
+    // Observer gets triggered when we add html to divContentAlerts, so we check for that here
+    if (!reinitialize) {
+        reinitialize = true;
+        return;
+    }
+
+    // Reinitialize
     initialize();
+});
 
-    // Create an observer to check for reload of alerts
-    const observer = new MutationObserver(function(mutations, observer) {
-        // Observer gets triggered when we add html to divContentAlerts, so we check for that here
-        if (!reinitialize) {
-            reinitialize = true;
-            return;
-        }
+// Observe the list of alerts
+observer.observe(document.querySelector('#divContentAlerts'), {
+    childList: true,
+    attributes: false,
+});
 
-        // Reinitialize
-        initialize();
+// Get element parents
+function parents(el, selector) {
+    const parents = [];
+    while ((el = el.parentNode) && el !== document) {
+        if (!selector || el.matches(selector)) parents.push(el);
+    }
+    return parents;
+}
+
+// Handle copy alert click
+document.addEventListener('click', event => {
+    if (!event.target.closest('.copy-alert')) {
+        return;
+    }
+
+    // Make sure we don't reinitialize the table
+    reinitialize = false;
+
+    // Clone the clicked button's parent table
+    const clonedTable = parents(event.target, 'table')[0].cloneNode(true);
+
+    // Remove the last tr (copy button tr)
+    clonedTable.querySelector('tr.custom-tr').remove();
+
+    // Remove alert-blink classes
+    clonedTable.querySelectorAll('a.alert-blink').forEach(elem => {
+        elem.classList.remove('alert-blink');
     });
 
-    // Observe the list of alerts
-    observer.observe($('#divContentAlerts').get(0), {
-        childList: true,
-        attributes: false,
-    });
+    // Append the table to the alerts overview
+    document.querySelector('#divContentAlerts').append(clonedTable);
 
-    // Handle copy alert click
-    $(document).on('click', '.copy-alert', function() {
-        // Make sure we don't reinitialize the table
-        reinitialize = false;
+    // Remove any selections from the current document
+    window.getSelection().removeAllRanges();
 
-        // Clone the clicked button's parent table
-        const $clonedTable = $(this).parents('table').clone();
+    // Create a range for the cloned table and select it
+    var range = document.createRange();
+    range.selectNode(clonedTable);
+    window.getSelection().addRange(range);
 
-        // Remove the last tr (copy button tr) and all alert-blink classes
-        $clonedTable.find('tbody tr').last().remove();
-        $clonedTable.find('a.alert-blink').removeClass('alert-blink');
+    // Execute the copy command
+    document.execCommand('copy');
 
-        // Append the table to the alerts overview
-        $('#divContentAlerts').append($clonedTable);
-
-        // Remove any selections from the current document
-        window.getSelection().removeAllRanges();
-
-        // Create a range for the cloned table and select it
-        var range = document.createRange();
-        range.selectNode($clonedTable.get(0));
-        window.getSelection().addRange(range);
-
-        // Execute the copy command
-        document.execCommand('copy');
-
-        // Remove the cloned table
-        $clonedTable.remove();
-    });
+    // Remove the cloned table
+    clonedTable.remove();
 });
